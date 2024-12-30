fe39e0ac96d03709b66c32e7421c77e706f64998
Rain <raingayming@gmail.com>
2024-12-30 17:17:50 +0000

    removed some debug code

--- unnamedrpggame/ui/inventory/scripts/inventory.gd
+++ unnamedrpggame/ui/inventory/scripts/inventory.gd
@@ -11,11 +11,6 @@ extends Node
 @export_group("debug")
 @export var debug_item: InventoryItem
 
-func _process(_delta):
-	#this is just debug remove later
-	if Input.is_action_just_pressed("ui_accept"):
-		debug_add()
-
 #debug code
 func debug_add():
 	add_item(debug_item)
